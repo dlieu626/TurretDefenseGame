ab588e6454c3ba438beffb36ff2f12fe69b39a8d
derrick lieu <dlieu626@gmail.com>
2020-02-25 16:13:54 -0800

    added upgraded turrets
    - Added upgraded missile launcher, laser beam turret
    
    // Still has problems with button positioning, need to debug probably at the end...

--- Assets/Scripts/NodeUI.cs
+++ Assets/Scripts/NodeUI.cs
@@ -1,16 +1,36 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NodeUI : MonoBehaviour
 {
     public GameObject ui;
     private Node target;
+    public TextMeshProUGUI upgradeCost;
+
+    public Button upgradeButton;
+    public TextMeshProUGUI sellCost;
 
     public void SetTarget (Node _target)
     {
         target = _target;
         transform.position = target.GetBuildPosition();
+
+        if (!target.isUpgraded)
+        {
+        upgradeCost.text = "-$ " + target.turretBluePrint.upgradeCost;
+        upgradeButton.interactable = true;
+        }
+        else {
+        upgradeCost.text = "DONE";
+        upgradeButton.interactable = false;
+
+        }
+
         ui.SetActive(true); 
+
+
     }
 
     public void Hide()
